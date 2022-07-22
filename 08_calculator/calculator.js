e50cb15fee81a30f9fa1d8ce0cda4fbb90df3c1d
jeffsusanto <susantojeffy@gmail.com>
2022-07-22 23:44:14 +0800

    finished power and factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,31 +1,44 @@
-const add = function(num1,num2) {
-	return num1 + num2;
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function(num1,num2) {
-	return num1 - num2;
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function(array) {
+const sum = function (array) {
   let sum = 0;
-  for(let i=0; i<array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     sum += array[i];
   }
-	return sum;
+  return sum;
 };
 
-const multiply = function(array) {
+const multiply = function (array) {
   return array.length
     ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
     : 0;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  let sum = 1
+  for (let i = 0; i < num2; i++) {
+    sum *= num1
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let sum
+  if (num === 0) {
+    return 1
+  }
+  sum = 1
+  for (let i = num; i > 0; i--) {
+    sum *= i
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
